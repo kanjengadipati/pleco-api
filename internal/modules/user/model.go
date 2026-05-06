6e0646f99d490992b4b1b0d7eeb5e9224dab692b
Heriyadi <adipatikanjeng@gmail.com>
2026-05-06 16:59:05 +0700

    fix fmt

--- internal/modules/user/model.go
+++ internal/modules/user/model.go
@@ -11,12 +11,12 @@ import (
 type User struct {
 	gorm.Model
 	Name               string
-	Email              string `gorm:"unique" json:"email"`
-	Password           string `json:"-"`
-	Role               string `json:"role"` // user / admin
-	RoleID             uint   `json:"role_id"`
+	Email              string          `gorm:"unique" json:"email"`
+	Password           string          `json:"-"`
+	Role               string          `json:"role"` // user / admin
+	RoleID             uint            `json:"role_id"`
 	RoleDetails        roleModule.Role `gorm:"foreignKey:RoleID" json:"role_details,omitempty"`
-	IsVerified         bool   `json:"is_verified"`
+	IsVerified         bool            `json:"is_verified"`
 	PasswordUpdatedAt  time.Time
 	AccessTokenVersion uint `gorm:"default:0" json:"-"`
 }
